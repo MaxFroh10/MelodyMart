db720c372dd908968bb12149c39416f4c1273c98
huret <huretantoine@gmail.com>
2024-05-21 19:28:29 +0200

    controller users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/password'
+  get 'user/email'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
