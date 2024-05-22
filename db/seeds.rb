50ba8bf8222da8db9bf63073dd29335a651d7a84
Maxence FROHLICHER <maxence.frohlicher@icloud.com>
2024-05-22 20:45:59 +0200

    Peaufinement des cartes et ajout d'instruments dans Seed pour test

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,19 +28,32 @@ Instrument.destroy_all
 Instrument.create!([
   {
   user_id: user1.id,
-  name: "banjo",
-  category: "guitare",
+  name: "Banjo",
+  category: "Guitare",
   price: 10,
   description: "ceci est un banjo",
   picture: "https://d1aeri3ty3izns.cloudfront.net/media/64/647602/600/preview.jpg"},
   {
   user_id: user1.id,
-  name: "batterie",
-  category: "percussion",
+  name: "Batterie",
+  category: "Percussion",
   price: 20,
-  description: "ceci est une batterie",
-  picture: "https://www.dwdrums.com/_next/image/?url=https%3A%2F%2Fa.storyblok.com%2Ff%2F165398%2F1644x1040%2F38d1e57402%2Fdw_key_drumrug_1644x1040.png&w=1080&q=75"
-}
-  ])
+  description: "Ceci est une batterie",
+  picture: "https://www.dwdrums.com/_next/image/?url=https%3A%2F%2Fa.storyblok.com%2Ff%2F165398%2F1644x1040%2F38d1e57402%2Fdw_key_drumrug_1644x1040.png&w=1080&q=75"},
+  {
+  user_id: user1.id,
+  name: "Harpe",
+  category: "Corde",
+  price: 150,
+  description: "Ceci est une harpe",
+  picture: "https://www.linstrumentarium.fr/harpes/549-large_default/gaia-acajou.jpg"},
+  {
+  user_id: user1.id,
+  name: "Violon",
+  category: "Vent",
+  price: 50,
+  description: "Ceci est un violon",
+  picture: "https://cdn.cultura.com/cdn-cgi/image/width=830/media/pim/pack-violon-massif-1-2-3700180000827_0.jpg"
+}])
 
 p "Created #{Instrument.count} instruments"
