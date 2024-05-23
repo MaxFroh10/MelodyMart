af33ab1dac67b0e30226b05da3f298b155c300a4
lewagon1540 <christophe.marco@net-c.fr>
2024-05-23 13:02:51 +0200

    index, show, edit, update, destroy, new+create no operational

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
-  resources :instruments, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:new, :create, :show]
+  resources :instruments do
+    resources :bookings, only: %i[new create show]
   end
 end
