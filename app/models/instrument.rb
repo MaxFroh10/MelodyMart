7589fd8efc52e956605382b87f9c6234d1faa3c9
huret <huretantoine@gmail.com>
2024-05-23 20:46:17 +0200

    booking model creation

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,9 +1,20 @@
 class Instrument < ApplicationRecord
   belongs_to :owner, class_name: "User"
+  has_many :bookings
 
   validates :name, presence: true
   validates :category, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :picture, presence: true
+
+
+  def available?(start_date, end_date)
+    bookings.each do |booking|
+      if (start_date..end_date).overlaps?(booking.start_date..booking.end_date)
+        return false
+      end
+    end
+    true
+  end
 end
