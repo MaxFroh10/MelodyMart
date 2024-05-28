56993178aa2513a5ea9fcebdcb32be9bf9d82339
Maxime Robert Colin <maximerobertcolin+lewagon@gmail.com>
2024-05-28 21:54:07 +0200

    changed models for users and instruments to enable easier validation

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,13 +1,15 @@
 class Instrument < ApplicationRecord
+  has_many :bookings
   has_one_attached :picture
   belongs_to :owner, class_name: "User"
-  has_many :bookings
+  belongs_to :category
+  belongs_to :subcategory
 
   validates :name, presence: true
   validates :category, presence: true
+  validates :subcategory, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
-
   validates :owner, presence: true
 
   def self.by_owner(owner)
