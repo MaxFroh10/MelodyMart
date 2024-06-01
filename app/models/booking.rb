789c402b76aa119ada9de80a85e99f1f80b0c625
lewagon1540 <christophe.marco@net-c.fr>
2024-06-01 17:02:37 +0200

    Validate no overlapping with bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,14 +4,25 @@ class Booking < ApplicationRecord
 
   validates :renter, presence: true
   validates :instrument, presence: true
-
-  #validate -> regarder si booking dates comprises dans un autre booking
   validates :start_date, presence: true, comparison: { greater_than_or_equal_to: Date.today }
   validates :end_date, presence: true, comparison: { greater_than_or_equal_to: :start_date }
   validates :status, presence: true, inclusion: { in: %w[pending accepted declined] }
+  validate :dates_do_not_overlap 
 
   # Method to find bookings where the instrument's owner is the given user
   def self.for_owner(user)
     joins(:instrument).where(instruments: { owner_id: user.id })
+  end
+
+  private
+
+  def dates_do_not_overlap
+    overlapping_bookings = Booking.where(instrument_id: instrument_id)
+                                  .where.not(id: id)
+                                  .where('start_date < ? AND end_date > ?', end_date, start_date)
+
+    return unless overlapping_bookings.exists?
+
+    errors.add(:base, 'Les dates de réservation se chevauchent avec une autre réservation.')
   end
 end
